aea4e4d3c601507660e57b37eec3ed5ec88c2523
Tiffy113 <94413603+Stephani13@users.noreply.github.com>
2022-03-30 17:42:11 -0400

    Add project files.

---
+++ embed video/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(function () {
+    var colors = ["#3DE056", "#FA9DAD", "#E0AF02", "#075DDE", "#FC7168"]
+
+    setInterval(function () {
+        var bgarray = Math.floor(Math.random() * colors.length);
+        var selectedcolor = colors[bgarray];
+        $("body").css("background", selectedcolor);
+    }, 3000);
+})
